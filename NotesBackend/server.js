c31e062bd8f31482a157e38f6217130391dd6c98
Deepanshurao2001 <raodeepanshu99@gmail.com>
2024-04-14 23:11:28 +0530

    created preview functionality on backend

--- NotesBackend/server.js
+++ NotesBackend/server.js
@@ -30,9 +30,32 @@ app.delete('/notes/delete/:id', (req, res) => {
 
   notes.splice(notes.findIndex(note => String(note.id) === String(reqDelNoteId)), 1);
   res.status(200).json({ data: notes });
-});
+})
+
+app.post('/notes/preview',(req, res) => {
+  let {id} = req.body
+  let note = notes.find((n) => String(n.id) == String(id) )
+  res.status(200).json({data:note})
+})
+
+app.post('/notes/preview/previewUpdate',(req, res) => {
+  let {noteId,updateBody} = req.body
+
+  let indexOfNoteToUpdate = notes.findIndex((n) => String(n.id) == String(noteId) )
+
+  let noteToUpdate = notes.find((n) => String(n.id) == String(noteId) )
+
+  noteToUpdate = {...noteToUpdate, ...updateBody}
+
+  notes[indexOfNoteToUpdate] = noteToUpdate
+
+  res.status(200).json({data:noteToUpdate})
+
+  
 
 
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
