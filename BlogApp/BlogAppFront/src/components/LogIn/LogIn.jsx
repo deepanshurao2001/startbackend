5831a916fa27551fea670403aa829b41e6f58088
Deepanshurao2001 <raodeepanshu99@gmail.com>
2024-04-18 23:15:31 +0530

    creating login page

--- BlogApp/BlogAppFront/src/components/LogIn/LogIn.jsx
+++ BlogApp/BlogAppFront/src/components/LogIn/LogIn.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './LogIn.css'
+import Box from '@mui/material/Box'
 
 export default function LogIn() {
     return (
@@ -9,8 +11,14 @@ export default function LogIn() {
             height: '100vh',
             width: '100vw',
             overflow: 'hidden',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
         }}>
-        
+        <Box sx={{display: 'flex', backgroundColor: "white" , borderRadius:'10px', opacity: '0.7' }}>
+            <Box sx={{display: 'flex', height: '500px', width: '380px', justifyContent:'center' }}> LogIn Part </Box>
+            <Box sx={{ backgroundImage: "url('https://cdn.pixabay.com/photo/2020/05/05/16/01/background-bokeh-5133696_1280.jpg')", backgroundSize: 'cover' ,height: '500px', width: '380px', margin:'10px', borderRadius: '10px'}}></Box>
+        </Box>
         </div>
     )
 }
