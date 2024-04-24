881416652642b9294d35f2826251aa978c22a40d
Deepanshurao2001 <raodeepanshu99@gmail.com>
2024-04-24 23:26:09 +0530

    adding create new User feature

--- BlogApp/BlogAppFront/src/components/LogIn/LogIn.jsx
+++ BlogApp/BlogAppFront/src/components/LogIn/LogIn.jsx
@@ -1,62 +1,127 @@
 import React from 'react'
 import './LogIn.css'
 import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider';
-import Chip from '@mui/material/Chip';
+import Divider from '@mui/material/Divider'
+import Chip from '@mui/material/Chip'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import Link from '@mui/material/Link';
-
+import Link from '@mui/material/Link'
+import ModalUnstyled from '../CreateAccount/CreateAccount'
 
 export default function LogIn() {
     return (
-        <div style={{
-            backgroundImage: 'url(https://cdn.pixabay.com/photo/2016/04/06/10/08/background-1311251_1280.jpg)',
-            backgroundSize: 'cover',
-            backgroundAttachment: 'fixed', // Fixed background
-            height: '100vh',
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
-        <Box  sx={{display: 'flex', backgroundColor: "white" , borderRadius:'10px', opacity: '0.7' }}>
-            <Box sx={{display: 'flex', flexDirection: 'column' ,height: '500px', width: '380px', justifyContent:'center', alignItems:'center' }}>
+        <div
+            style={{
+                backgroundImage:
+                    'url(https://cdn.pixabay.com/photo/2016/04/06/10/08/background-1311251_1280.jpg)',
+                backgroundSize: 'cover',
+                backgroundAttachment: 'fixed', // Fixed background
+                height: '100vh',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    backgroundColor: 'white',
+                    borderRadius: '10px',
+                    opacity: '0.7',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '500px',
+                        width: '380px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography
+                            variant="h2"
+                            gutterBottom
+                            sx={{ color: 'Pink', marginBottom: '-10px' }}
+                        >
+                            Welcome
+                        </Typography>
+                        <Typography
+                            variant="caption"
+                            display="block"
+                            gutterBottom
+                            sx={{
+                                marginTop: '0px',
+                                marginBottom: '50px',
+                                display: 'flex',
+                                flexDirection: 'row',
+                            }}
+                        >
+                            New User ? <ModalUnstyled />
+                        </Typography>
+                    </Box>
 
+                    <Box
+                        component="form"
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            '& > :not(style)': { m: 1, width: '25ch' },
+                            marginBottom: '20px',
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <TextField label="Username" variant="filled" />
+                        <TextField
+                            label="Password"
+                            variant="filled"
+                            type="password"
+                        />
+                        <Button
+                            sx={{
+                                backgroundColor: '#FBBCE9',
+                                '&:hover': { backgroundColor: 'LightPink' },
+                                borderRadius: '20px',
+                                color: 'black',
+                            }}
+                        >
+                            LogIn
+                        </Button>
+                    </Box>
 
-                <Box sx={{display:'flex', flexDirection:'column',}}>
-                <Typography variant="h2" gutterBottom sx={{color:'Pink', marginBottom: '-10px'}}>
-                   Welcome
-                </Typography>
-                <Typography variant="caption" display="block" gutterBottom sx={{marginTop: '0px', marginBottom: '50px'}}>
-                New User ? <Link href="https://www.google.com/">SignUp</Link>
-                </Typography>
-                </Box>
+                    <Divider sx={{ width: '300px' }}>
+                        <Chip label="OR" size="small" sx={{ width: '200px' }} />
+                    </Divider>
 
-
-                <Box component="form" sx={{display:'flex', flexDirection:'column',  '& > :not(style)': { m: 1, width: '25ch' }, marginBottom:'20px'}}  noValidate autoComplete="off">
-                    <TextField  label="Username" variant="filled" />
-                    <TextField  label="Password" variant="filled" type='password'/>
-                    <Button sx={{backgroundColor:'#FBBCE9', '&:hover': {backgroundColor: 'LightPink'}, borderRadius:'20px', color:'black' }}>
-                      LogIn
+                    <Button
+                        sx={{
+                            backgroundColor: '#FBBCE9',
+                            '&:hover': { backgroundColor: 'LightPink' },
+                            borderRadius: '20px',
+                            margin: '20px',
+                            color: 'black',
+                        }}
+                    >
+                        Continue with Google
                     </Button>
                 </Box>
-
-
-
-
-
-
-            <Divider sx={{width:'300px'}}>
-            <Chip label="OR" size="small"sx={{ width:"200px"}} />
-            </Divider>    
-            
-            <Button sx={{backgroundColor:'#FBBCE9', '&:hover': {backgroundColor: 'LightPink'}, borderRadius:'20px' , margin:'20px', color:'black'}}>Continue with Google</Button>
-
+                <Box
+                    sx={{
+                        backgroundImage:
+                            "url('https://cdn.pixabay.com/photo/2020/05/05/16/01/background-bokeh-5133696_1280.jpg')",
+                        backgroundSize: 'cover',
+                        height: '500px',
+                        width: '380px',
+                        margin: '10px',
+                        borderRadius: '10px',
+                    }}
+                ></Box>
             </Box>
-            <Box sx={{ backgroundImage: "url('https://cdn.pixabay.com/photo/2020/05/05/16/01/background-bokeh-5133696_1280.jpg')", backgroundSize: 'cover' ,height: '500px', width: '380px', margin:'10px', borderRadius: '10px'}}></Box>
-        </Box>
         </div>
     )
 }
--- BlogApp/BlogAppFront/src/components/MainSpace/MainSpace.jsx
+++ BlogApp/BlogAppFront/src/components/MainSpace/MainSpace.jsx
@@ -1,30 +1,30 @@
-import * as React from 'react';
-import {useState} from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import Recent from '../MainSpaceLayout/Recent';
-import Trending from '../MainSpaceLayout/Trending';
-import UserBlog from '../UserBlog/UserBlog';
-import Profile from '../Profile/Profile';
+import * as React from "react";
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import Recent from "../MainSpaceLayout/Recent";
+import Trending from "../MainSpaceLayout/Trending";
+import UserBlog from "../UserBlog/UserBlog";
+import Profile from "../Profile/Profile";
 
-const pages = ['Recent', 'Trending', 'MyBlogs'];
-const settings = ['Profile', 'Logout'];
+const pages = ["Recent", "Trending", "MyBlogs"];
+const settings = ["Profile", "Logout"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const [currentPage, setCurrentPage] = useState("Recent")
+  const [currentPage, setCurrentPage] = useState("Recent");
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -43,155 +43,168 @@ function ResponsiveAppBar() {
 
   return (
     <Box>
-          <AppBar position="static">
-            <Container maxWidth="xl">
-              <Toolbar disableGutters>
-                <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                <Typography
-                  variant="h6"
-                  noWrap
-                  component="a"
-                  href="#app-bar-with-responsive-menu"
-                  sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    fontFamily: 'monospace',
-                    fontWeight: 700,
-                    letterSpacing: '.3rem',
-                    color: 'inherit',
-                    textDecoration: 'none',
-                  }}
-                >
-                  LOGO
-                </Typography>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
 
-                {/* Search Area */}
-                <Box sx={{ marginRight:'50px'}}>
-                  <input type="text" placeholder="Search..." style={{ width: '100%', padding: '8px', borderRadius: '4px', border: '1px solid #ccc' }} />
-                </Box>
-
-                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                  <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleOpenNavMenu}
-                    color="inherit"
-                  >
-                    <MenuIcon />
-                  </IconButton>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorElNav}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'left',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'left',
-                    }}
-                    open={Boolean(anchorElNav)}
-                    onClose={handleCloseNavMenu}
-                    sx={{
-                      display: { xs: 'block', md: 'none' },
-                    }}
-                  >
-                    {pages.map((page) => (
-                      <MenuItem key={page} onClick={() => {setCurrentPage(page)}}>
-                        <Typography textAlign="center">{page}</Typography>
-                      </MenuItem>
-                    ))}
-                  </Menu>
-                </Box>
-                <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                <Typography
-                  variant="h5"
-                  noWrap
-                  component="a"
-                  href="#app-bar-with-responsive-menu"
-                  sx={{
-                    mr: 2,
-                    display: { xs: 'flex', md: 'none' },
-                    flexGrow: 1,
-                    fontFamily: 'monospace',
-                    fontWeight: 700,
-                    letterSpacing: '.3rem',
-                    color: 'inherit',
-                    textDecoration: 'none',
-                  }}
-                >
-                  LOGO
-                </Typography>
-
-
-                {/* Navigation Menu */}
-
-                
-                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                  {pages.map((page) => (
-                    <Button
-                      key={page}
-                      onClick={() => setCurrentPage(page)}
-                      sx={{ my: 2, color: 'white', display: 'block' }}
-                    >
-                      {page}
-                    </Button>
-                  ))}
-                </Box>
-
-
-                {/* Profile and menu */}
-
-                <Box sx={{ flexGrow: 0 }}>
-                  <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                    </IconButton>
-                  </Tooltip>
-                  <Menu
-                    sx={{ mt: '45px' }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                  >
-                    {settings.map((setting) => (
-                      <MenuItem key={setting} onClick={() => {setCurrentPage(setting)}}>
-                        <Typography textAlign="center">{setting}</Typography>
-                      </MenuItem>
-                    ))}
-                  </Menu>
-                </Box>
-              </Toolbar>
-            </Container>
-          </AppBar>
-            <Box>
-
-                      {/* Pages */}
-        {/* coontional render */}
-                      {currentPage === "Recent" && <Recent/>}                
-                      {currentPage === "Trending" && <Trending/>}
-                      {currentPage === "MyBlogs" && <UserBlog/>}
-                      {currentPage === "Profile" && <Profile/>}
-
+            {/* Search Area */}
+            <Box sx={{ marginRight: "50px" }}>
+              <input
+                type="text"
+                placeholder="Search..."
+                style={{
+                  width: "100%",
+                  padding: "8px",
+                  borderRadius: "4px",
+                  border: "1px solid #ccc",
+                }}
+              />
             </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem
+                    key={page}
+                    onClick={() => {
+                      setCurrentPage(page);
+                    }}
+                  >
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+
+            {/* Navigation Menu */}
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={() => setCurrentPage(page)}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            {/* Profile and menu */}
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem
+                    key={setting}
+                    onClick={() => {
+                      setCurrentPage(setting);
+                    }}
+                  >
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box>
+        {/* Pages */}
+        {/* coontional render */}
+        {currentPage === "Recent" && <Recent />}
+        {currentPage === "Trending" && <Trending />}
+        {currentPage === "MyBlogs" && <UserBlog />}
+        {currentPage === "Profile" && <Profile />}
+      </Box>
     </Box>
   );
 }
 export default ResponsiveAppBar;
-
 
 /*
 
